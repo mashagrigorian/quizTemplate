c734ba125a45bfcdcd9d014ce43b52aacb798dd7
Gevorg <mirzoyan.gevorg.23@gmail.com>
2023-05-17 18:17:43 +0400

    change answers true false bgcolor

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ nextButton.addEventListener('click', () => {
 function startGame () {
 console.log('Started')
 startButton.classList.add('hide')
+nextButton.classList.remove('hide')
 shuffleQusetions = questions.sort(() => Math.random() -0.5)
 currentQuestionIndex = 0
 score = 0
@@ -38,7 +39,6 @@ function showQuestion (question) {
         answersButtonElement.appendChild(button)
     })
 }
-
 function setNextQuestion () {
     resetState()
     showQuestion(shuffleQusetions[currentQuestionIndex])
@@ -49,3 +49,134 @@ function resetState () {
         answersButtonElement.removeChild(answersButtonElement.firstChild)
     }
 }
+
+function selectAnswer (e) {
+    const selectButton = e.target
+    const correct = selectButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answersButtonElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (correct) {
+        score++
+        selectButton.style.backgroundColor = 'green'
+        // scoreElement.innerText = `Score ${score}`
+    } else {
+        selectButton.style.backgroundColor = 'red'
+
+    }
+    if (shuffleQusetions.length > currentQuestionIndex +1) {
+        nextButton.classList.remove('hide')
+    }else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+        questtionContainerElements.classList.add('hide')
+        scoreElement.innerText = `You scored ${score} out of ${questions.length}`
+    }
+    if (nextButton.addEventListener('click', () => {
+
+    }))
+    nextButton.classList.remove('hide')
+}
+function setStatusClass (element,correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+function clearStatusClass (element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+const questions = [
+    {
+        question: 'Who is making the Web standards?',
+        answers:[
+            {text:'Microsoft', correct: false},
+            {text: 'Mozilla', correct: false},
+            {text: 'Google', correct: false},
+            {text: 'The World Wide Web Consortium', correct: true},
+        ] 
+    },
+    {
+        question: 'Choose the correct HTML element for the largest heading:',
+        answers:[
+            {text:'head', correct: false},
+            {text: 'heading', correct: false},
+            {text: 'h1', correct: true},
+            {text: 'p', correct: false},
+        ] 
+    },
+    {
+        question: 'Which character is used to indicate an end tag??',
+        answers:[
+            {text:'.', correct: false},
+            {text: '>', correct: false},
+            {text: ')', correct: false},
+            {text: '/', correct: true},
+        ] 
+    },
+    {
+        question: 'Which of these elements are all <table> elements?',
+        answers:[
+            {text:'table, tr, tt', correct: false},
+            {text: 'table, head, tfoot', correct: false},
+            {text: 'table, tr, td', correct: true},
+            {text: 'thead, body, tr', correct: false},
+        ] 
+    },
+    {
+        question: 'console.log(typeof typeof 1);',
+        answers:[
+            {text:'string', correct: true},
+            {text: 'number', correct: false},
+            {text: '1', correct: false},
+            {text: 'true', correct: false},
+        ] 
+    },
+    {
+        question: 'Javascript is an object oriented language?',
+        answers:[
+            {text:'False', correct: false},
+            {text: 'true', correct: true},
+        ]
+    },
+    {
+        question: 'What is the alternate name for Java script?',
+        answers:[
+            {text:'LimeScript', correct: false},
+            {text: 'Both a and d', correct: false},
+            {text: 'ECMScript', correct: false},
+            {text: 'ECMAScript ', correct: true},
+        ] 
+    },
+    {
+        question: 'To insert a JavaScript into an HTML page, which tag is used?',
+        answers:[
+            {text:' < script=â€™javaâ€™>', correct: false},
+            {text: '< javascript>', correct: false},
+            {text: '< script> ', correct: true},
+            {text: '< js> ', correct: false},
+        ] 
+    },
+    {
+        question: 'Which of the following type of variable is visible everywhere in your JavaScript code?',
+        answers:[
+            {text:' global variable', correct: true},
+            {text: ' local variable', correct: false},
+            {text: 'Both of the above.', correct: false},
+            {text: 'None of the above. ', correct: false},
+        ] 
+    },
+    {
+        question: 'What language defines the behavior of a web page?',
+        answers:[
+            {text:'  HTML ', correct: false},
+            {text: ' CSS', correct: false},
+            {text: ' XML', correct: false},
+            {text: 'Java Script ', correct: true},
+        ] 
+    },
+]

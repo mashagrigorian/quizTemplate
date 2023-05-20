73f946823c91d9cccec8a0f112d35186701404b4
DavitMouradyan <davidmuradyan0422@gmail.com>
2023-05-20 21:45:16 +0400

    js part 2

--- js/app.js
+++ js/app.js
@@ -49,3 +49,129 @@ function resetState () {
         answersButtonElement.removeChild(answersButtonElement.firstChild)
     }
 }
+
+function selectAnswer (e) {
+    const selectButton = e.target
+    const correct = selectButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answersButtonElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (correct) {
+        score++
+        // scoreElement.innerText = `Score ${score}`
+    }
+    if (shuffleQusetions.length > currentQuestionIndex +1) {
+        nextButton.classList.remove('hide')
+    }else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+        questtionContainerElements.classList.add('hide')
+        scoreElement.innerText = `You scored ${score} out of ${questions.length}`
+    }
+    if (nextButton.addEventListener('click', () => {
+    }))
+    nextButton.classList.remove('hide')
+}
+function setStatusClass (element,correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+function clearStatusClass (element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+const questions = [
+    {
+        question: 'Who is making the Web standards?',
+        answers:[
+            {text:'Microsoft', correct: false},
+            {text: 'Mozilla', correct: false},
+            {text: 'Google', correct: false},
+            {text: 'The World Wide Web Consortium', correct: true},
+        ]
+    },
+    {
+        question: 'Choose the correct HTML element for the largest heading:',
+        answers:[
+            {text:'head', correct: false},
+            {text: 'heading', correct: false},
+            {text: 'h1', correct: true},
+            {text: 'p', correct: false},
+        ]
+    },
+    {
+        question: 'Which character is used to indicate an end tag??',
+        answers:[
+            {text:'.', correct: false},
+            {text: '>', correct: false},
+            {text: ')', correct: false},
+            {text: '/', correct: true},
+        ]
+    },
+    {
+        question: 'Which of these elements are all <table> elements?',
+        answers:[
+            {text:'table, tr, tt', correct: false},
+            {text: 'table, head, tfoot', correct: false},
+            {text: 'table, tr, td', correct: true},
+            {text: 'thead, body, tr', correct: false},
+        ]
+    },
+    {
+        question: 'console.log(typeof typeof 1);',
+        answers:[
+            {text:'string', correct: true},
+            {text: 'number', correct: false},
+            {text: '1', correct: false},
+            {text: 'true', correct: false},
+        ]
+    },
+    {
+        question: 'Javascript is an object oriented language?',
+        answers:[
+            {text:'False', correct: false},
+            {text: 'true', correct: true},
+        ]
+    },
+    {
+        question: 'What is the alternate name for Java script?',
+        answers:[
+            {text:'LimeScript', correct: false},
+            {text: 'Both a and d', correct: false},
+            {text: 'ECMScript', correct: false},
+            {text: 'ECMAScript ', correct: true},
+        ]
+    },
+    {
+        question: 'To insert a JavaScript into an HTML page, which tag is used?',
+        answers:[
+            {text:' < script=â€™javaâ€™>', correct: false},
+            {text: '< javascript>', correct: false},
+            {text: '< script> ', correct: true},
+            {text: '< js> ', correct: false},
+        ]
+    },
+    {
+        question: 'Which of the following type of variable is visible everywhere in your JavaScript code?',
+        answers:[
+            {text:' global variable', correct: true},
+            {text: ' local variable', correct: false},
+            {text: 'Both of the above.', correct: false},
+            {text: 'None of the above. ', correct: false},
+        ]
+    },
+    {
+        question: 'What language defines the behavior of a web page?',
+        answers:[
+            {text:'  HTML ', correct: false},
+            {text: ' CSS', correct: false},
+            {text: ' XML', correct: false},
+            {text: 'Java Script ', correct: true},
+        ]
+    },
+]
